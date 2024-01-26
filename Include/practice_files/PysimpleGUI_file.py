8748f434beecb7655a025ef2a617f6a9c4cb88f0
ritigit7 <ritikm01092003@gmail.com>
2024-01-26 23:25:51 +0530

    pysimple files edit

--- Include/practice_files/PysimpleGUI_file.py
+++ Include/practice_files/PysimpleGUI_file.py
@@ -1,24 +1,51 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('Enter the first number:'), sg.InputText()],
-          [sg.Text('Enter the second number:'), sg.InputText()],
-          [sg.Button('Add'), sg.Button('Cancel')]]
-
-window = sg.Window('Add Two Numbers', layout)
-
+layout=[[sg.Text(text="Hello world",justification='center')],
+        [sg.Button('click')]]
+window=sg.Window("Trial",layout,size=(200,100))
 while True:
-    event, values = window.read()
-    if event in (None, 'Cancel'):
+    event,value=window.read()
+    # print(event,value)
+    if event in (None,'Exit'):
         break
-    try:
-        num1 = int(values[0])
-        num2 = int(values[1])
-        result = num1 + num2
-        sg.popup(f'The sum of {num1} and {num2} is {result}')
-    except ValueError:
-        sg.popup('Please enter valid integers')
-
+    if event=='click':
+        sg.popup('ok')
+        fl=open('D:/python programs/All/.venv/Include/practice_files/temp.txt')
+        txt=fl.read()
+        print( sg.popup_scrolled(txt))
+        print( sg.popup_get_file(txt,  title="File selector"))
+        fl.close()
+        # sg.popup_auto_close('This window will Autoclose')
 window.close()
+
+
+
+
+
+
+
+
+
+
+# layout = [[sg.Text('Enter the first number:'), sg.InputText()],
+#           [sg.Text('Enter the second number:'), sg.InputText()],
+#           [sg.Button('Add'), sg.Button('Cancel')]]
+
+# window = sg.Window('Add Two Numbers', layout)
+
+# while True:
+#     event, values = window.read()
+#     if event in (None, 'Cancel'):
+#         break
+#     try:
+#         num1 = int(values[0])
+#         num2 = int(values[1])
+#         result = num1 + num2
+#         sg.popup(f'The sum of {num1} and {num2} is {result}')
+#     except ValueError:
+#         sg.popup('Please enter valid integers')
+
+# window.close()
 
 
 
